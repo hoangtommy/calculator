14277a5acd937f0ac6ced23e1d8be17f1e54f67f
Tommy <t@tommyhoang.com>
2018-08-01 13:49:32 -0700

    basic calculator is working

--- script.js
+++ script.js
@@ -1,63 +1,101 @@
 //create add function
-function add(...arguments) {
-	let result = arguments.reduce((total, num) => {
+function add(...nums) {
+	let result = nums.reduce((total, num) => {
 		return total + num;
 	});
 	return result;
 }
 
 //create subtract function
-function subtract(...arguments) {
-	let result = arguments.reduce((total, num) => {
+function subtract(...nums) {
+	let result = nums.reduce((total, num) => {
 		return total - num;
 	});
 	return result;
 }
 
 //create product function
-function multiply(...arguments) {
-	let result = arguments.reduce((total, num) => {
+function multiply(...nums) {
+	let result = nums.reduce((total, num) => {
 		return total * num;
 	});
 	return result;
 }
 
 //create divid
-function divide(...arguments) {
-	let result = arguments.reduce((total, num) => {
+function divide(...nums) {
+	let result = nums.reduce((total, num) => {
 		return total / num;
 	});
+	if (result === Infinity) {
+		display.innerHTML = 'you can\'t divide by zero';
+	}
 	return result;
 }
 
-//function that takes two numbers and an operator. performs the math on the two numbers
+//takes two numbers and an operator. performs the math on the two numbers and displays answer
 function operate(operator, num1, num2) {
-	return operator(num1, num2);
+	if (typeof operator === 'string') {
+		switch (operator) {
+			case 'add':
+			return add(num1, num2);
+			case 'subtract':
+			return subtract(num1, num2);
+			case 'multiply':
+			return multiply(num1, num2);
+			case 'divide':
+			return divide(num1, num2);
+		}
+	}
+	return display.innerHTML = operator(num1, num2);
 }
 
 //global variables
 let display = document.querySelector('#display');
-let storedNumber = 0;
 let storedOperator;
+let storedNumber = '';
+let storedNumber2 = '';
 
-//display number 
-function displayNumbers() {
-	let btns = document.querySelectorAll('.numbers');
-	btns.forEach(button => button.addEventListener('click', (e) => {
-		storedNumber = e.target.innerText;
-		display.innerHTML += storedNumber;
-	}));
-	return storedNumber;
+//display number
+let btns = document.querySelectorAll('.numbers');
+btns.forEach(button => button.addEventListener('click', (e) => {
+	display.innerHTML = '';
+	display.innerHTML += e.target.innerText;
+	getNumber();
+}));
+
+//store numbers and return 
+function getNumber() {
+	if (storedNumber !== '') {
+		return storedNumber2 = Number(display.innerHTML);
+	}
+	return storedNumber = Number(display.innerHTML);
 }
 
-//display operator
-function displayOperator() {
-	let opBtns = document.querySelectorAll('.operators');
-	opBtns.forEach(button => button.addEventListener('click', (e) => {
-		storedOperator = e.target.innerText;
-		display.innerHTML += storedOperator;
-	}));
+//display and store operator
+let opBtns = document.querySelectorAll('.ops');
+opBtns.forEach(button => button.addEventListener('click', (e) => {
+	display.innerHTML = '';
+	storedOperator = e.target.id;
+	display.innerHTML += e.target.innerText;
+	getOperator();
+}));
+
+//clear display
+let clearBtn = document.querySelector('.clear');
+clearBtn.addEventListener('click', (e) => {
+	display.innerHTML = '';
+	storedNumber = '';
+	storedNumber2 = '';
+	storedOperator = '';
+});
+
+//display and store operator
+function getOperator() {
+	return storedOperator;
 }
 
-displayNumbers();
-displayOperator();
+let equalsBtn = document.getElementById('equal');
+equalsBtn.addEventListener('click', (e) => {
+	display.innerHTML = operate(storedOperator, storedNumber, storedNumber2);
+});
