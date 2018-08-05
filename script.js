82aebe96df2725af9c5422705625811b52caae11
Tommy <t@tommyhoang.com>
2018-08-05 11:45:25 -0700

    add functions for percent, negative/positive, period

--- script.js
+++ script.js
@@ -66,6 +66,42 @@ clearBtn.addEventListener('click', (e) => {
 	storedOperator = '';
 });
 
+//makes number negative or positive
+let negPosBtn = document.getElementById('negative');
+negPosBtn.addEventListener('click', () => {
+	if (display.innerHTML == storedNumber) {
+		storedNumber *= -1;
+		displayScreen(storedNumber);
+	} else if (display.innerHTML == storedNumber2) {
+		storedNumber2 *= -1;
+		displayScreen(storedNumber2);
+	}
+});
+
+//makes number a percentage
+let percentBtn = document.getElementById('percent');
+percentBtn.addEventListener('click', () => {
+	if (display.innerHTML == storedNumber) {
+		storedNumber /= 100;
+		displayScreen(storedNumber);
+	} else if (display.innerHTML == storedNumber2) {
+		storedNumber2 /= 100;
+		displayScreen(storedNumber2);
+	}
+});
+
+//adds dot to number
+let dotBtn = document.querySelector('#point');
+dotBtn.addEventListener('click', () => {
+	if (display.innerHTML == storedNumber) {
+		storedNumber = storedNumber + '.';
+		displayScreen(storedNumber);
+	} else if (display.innerHTML == storedNumber2) {
+		storedNumber2 = storedNumber2 + '.';
+		displayScreen(storedNumber2);
+	}
+});
+
 let equalsBtn = document.getElementById('equal');
 equalsBtn.addEventListener('click', (e) => {
 	if (storedNumber2 == '' || storedOperator == '') {
